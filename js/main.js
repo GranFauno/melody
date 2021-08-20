a15456ee0d9641c5fef7f5ad7d954196031aa578
GranFauno <p1xeler1239587@gmail.com>
2021-08-20 17:32:00 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     var currentFloor = 2; // переменная, где хранится текущий этаж
     var counterUp = $(".counter-button--up"); // кнопка увеличения этажа
     var counterDown = $(".counter-button--down"); // кнопка уменьшения этажа
+    var modal=$(".modal");
+    var modalCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats");
     
     //функция при наведении курсором на этаж
     $(".home-image path").on("mouseover", function(){
@@ -10,6 +13,18 @@ $(document).ready(function () {
         $(".counter").text(currentFloor);// записываем значение этажа в счетчик справа
     });
     
+    $(".home-image path").on("click", function () {
+        modal.toggleClass("is-open")
+      });
+
+    modalCloseButton.on("click", function(){
+        modal.toggleClass("is-open");
+    });
+    
+    viewFlatsButton.on("click", function () {
+        modal.toggleClass("is-open");
+    });
+
     counterUp.on("click", function(){ // отслеживаем клик по кнопке вверх
         if (currentFloor < 18){ // проверяем значение этажа
             currentFloor++; 
