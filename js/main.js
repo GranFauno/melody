733205c9aab71749070c9af1e8fac99c95f46203
GranFauno <p1xeler1239587@gmail.com>
2021-08-20 11:50:42 +0300

    create: counter

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 $(document).ready(function () {
-    var currentFloor = 2;
-    var counterUp = $(".counter-button--up");
-    var counterDown = $(".counter-button--down");
+    var currentFloor = 2; // переменная, где хранится текущий этаж
+    var counterUp = $(".counter-button--up"); // кнопка увеличения этажа
+    var counterDown = $(".counter-button--down"); // кнопка уменьшения этажа
+    
+    //функция при наведении курсором на этаж
     $(".home-image path").on("mouseover", function(){
-        $(".home-image path").removeClass("current-floor")
-        currentFloor=$(this).attr("data-floor");
-        $(".counter").text(currentFloor);
+        $(".home-image path").removeClass("current-floor") // удаляем активный класс у этаже
+        currentFloor=$(this).attr("data-floor"); //получаем значение текущего этажа
+        $(".counter").text(currentFloor);// записываем значение этажа в счетчик справа
     });
     
-    counterUp.on("click", function(){
-        if (currentFloor < 18){
-            currentFloor++;
-        usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGroupping: false});
+    counterUp.on("click", function(){ // отслеживаем клик по кнопке вверх
+        if (currentFloor < 18){ // проверяем значение этажа
+            currentFloor++; 
+        usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGroupping: false}); // форматирование переменной с этажом
         $(".counter").text(usCurrentFloor);
         $(".home-image path").removeClass("current-floor");
-        $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+        $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
         }
     });
     counterDown.on("click", function(){
